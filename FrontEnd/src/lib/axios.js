cc67aa8d61cb99989f1669a4f9951f7e267e448a
Siddhesh Gite <148460427+Siddhesh200419@users.noreply.github.com>
2025-11-27 15:19:25 +0530

    Refactor axios base URL configuration

--- FrontEnd/src/lib/axios.js
+++ FrontEnd/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const BASE_API_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"                        // LOCAL backend
+    : "https://chatty-backend-1fu6.onrender.com/api";    // RENDER backend
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chatty-backend-1fu6.onrender.com/api" : "/api",
+  baseURL: BASE_API_URL,
   withCredentials: true,
 });
