62a9d315d813d5c0daffd6d444bc12aa80bf3945
Siddhesh Gite <148460427+Siddhesh200419@users.noreply.github.com>
2025-11-27 16:12:33 +0530

    Update index.js

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -18,8 +18,10 @@ app.use(cors({
 }));
 
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
+
 
 
 app.use("/api/auth", authRoutes);
